b1e46b7839b56db2e38d1cf2d9c862e3c8c4c41f
utkbkts <utkbktss5@gmail.com>
2025-03-22 20:40:22 +0300

    feat : permission added

--- backend/middlewares/permissions.ts
+++ backend/middlewares/permissions.ts
@@ -10,8 +10,51 @@ const isAdmin = rule({ cache: "contextual" })(async (parent, args, context) => {
   return context?.user?.role?.includes("admin");
 });
 
-export const permissions = shield({
-  Query: {
-    me: and(isAuthenticated, isAdmin),
+export const permissions = shield(
+  {
+    Query: {
+      getAllBookings: isAuthenticated,
+      getAllUsers: and(isAuthenticated, isAdmin),
+      me: isAuthenticated,
+      logout: isAuthenticated,
+      getBookingById: isAuthenticated,
+      myBookings: isAuthenticated,
+      getDashboardStats: and(isAuthenticated, isAdmin),
+
+      getAllReviews: isAuthenticated,
+      canReview: isAuthenticated,
+    },
+    Mutation: {
+      createCar: and(isAuthenticated, isAdmin),
+      updateCar: and(isAuthenticated, isAdmin),
+      deleteCarImage: and(isAuthenticated, isAdmin),
+      deleteCar: and(isAuthenticated, isAdmin),
+
+      updateBooking: isAuthenticated,
+      deleteBooking: and(isAuthenticated, isAdmin),
+      createBooking: isAuthenticated,
+
+      updateUserProfile: isAuthenticated,
+      updatePassword: isAuthenticated,
+      updateAvatar: isAuthenticated,
+      updateUser: and(isAuthenticated, isAdmin),
+      deleteUser: and(isAuthenticated, isAdmin),
+
+      createCoupon: and(isAuthenticated, isAdmin),
+      updateCoupon: and(isAuthenticated, isAdmin),
+      deleteCoupon: and(isAuthenticated, isAdmin),
+
+      createFaq: and(isAuthenticated, isAdmin),
+      updateFaq: and(isAuthenticated, isAdmin),
+      deleteFaq: and(isAuthenticated, isAdmin),
+
+      stripeCheckoutSession: and(isAuthenticated, isAdmin),
+
+      createUpdateReview: isAuthenticated,
+      deleteReview: and(isAuthenticated, isAdmin),
+    },
   },
-});
+  {
+    debug: true,
+  }
+);
